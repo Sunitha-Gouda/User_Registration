6dacd0a3297c4fe3c1c51bd4d22b61eb2990641b
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-21 12:59:16 +0530

    User Email Validation

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -10,11 +10,24 @@ regex="^([[:upper:]]{1})([[:lower:]]{2})([a-z]*)$"
 		echo " Your Name does not match the criteria ***"
 	fi
 }
+emailValidation() {
+emailCheck="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}$"
+
+	if [[ $user_email =~ $emailCheck ]]
+	then
+		echo "  Valid Email  "
+	else
+		echo " Invalid Email "
+	fi
+}
 userRegistration(){
 echo " ***Note that your name starts with Cap and minimum 3 characters"
 read -p  " Enter your valid first name for User Registration Process :: " user_firstname
 read -p  " Enter your valid first name for User Registration Process :: " user_lastname
-
 nameValidation
+
+read -p  " Enter your valid Email for any future purpose ::" user_email
+emailValidation
+
 }
 userRegistration
