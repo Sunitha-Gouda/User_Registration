acef7ae3bf39e51768b9c94119bde53a4723f308
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-21 13:09:02 +0530

    Phone Number Validation

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -20,6 +20,16 @@ emailCheck="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}$"
 		echo " Invalid Email "
 	fi
 }
+numberValidation() {
+numberValid="^[+]{1}[0-9]{1,3}[[:space:]]{1}[0-9]{10}$"
+
+	if [[ $user_number =~ $numberValid ]]
+	then
+		echo "  Mobile number is verified "
+	else
+		echo " Mobile number your have entered is incorrect!!!! "
+	fi
+}
 userRegistration(){
 echo " ***Note that your name starts with Cap and minimum 3 characters"
 read -p  " Enter your valid first name for User Registration Process :: " user_firstname
@@ -29,5 +39,8 @@ nameValidation
 read -p  " Enter your valid Email for any future purpose ::" user_email
 emailValidation
 
+echo "***Country code followed  by your 10 digit  mobile number Ex.+91 8152813488 "
+read -p  " Enter your valid Mobile number for updating information :: " user_number
+numberValidation
 }
 userRegistration
