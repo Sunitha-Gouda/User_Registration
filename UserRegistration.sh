b0c4a93644d6280a88ce4eae42d7e27605b12458
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-21 12:51:54 +0530

    User First Name and Last Name Validation check

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -3,16 +3,18 @@
 nameValidation() {
 regex="^([[:upper:]]{1})([[:lower:]]{2})([a-z]*)$"
 
-	if [[ $user_firstname =~ $regex ]]
+	if [[ $user_firstname =~ $regex && $user_lastname =~ $regex  ]]
 	then
-		echo " First name is valid  "
+		echo " Your First name  and Last name are valid  "
 	else
-		echo " First name does not match the criteria ***"
+		echo " Your Name does not match the criteria ***"
 	fi
 }
 userRegistration(){
 echo " ***Note that your name starts with Cap and minimum 3 characters"
 read -p  " Enter your valid first name for User Registration Process :: " user_firstname
+read -p  " Enter your valid first name for User Registration Process :: " user_lastname
+
 nameValidation
 }
 userRegistration
